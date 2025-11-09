085caeb80ea543cd674a5f369fa9578468c857e0
Alejo <20245281@esen.edu.sv>
2025-11-09 11:57:54 -0600

    a

--- Guia6_20245281/js/formulario.js
+++ Guia6_20245281/js/formulario.js
@@ -35,3 +35,142 @@ const limpiarForm = () => {
     inputNombrePais.value = "";
     inputNombre.focus();
 }
+
+const addPaciente = function () {
+    let nombre = inputNombre.value;
+    let apellido = inputApellido.value;
+    let fechaNacimiento = inputFechaNacimiento.value;
+    let sexo =
+        inputRdMasculino.checked == true
+            ? "Hombre"
+            : inputRdFemenino.checked == true
+            ? "Mujer"
+            : "";
+    let pais = cmbPais.value;
+    let labelPais = cmbPais.options[cmbPais.selectedIndex].text;
+    let direccion = inputDireccion.value;
+
+    if (
+        nombre != "" &&
+        apellido != "" &&
+        fechaNacimiento != "" &&
+        sexo != "" &&
+        pais != 0 &&
+        direccion != ""
+    ) {
+        //Agregando informacion al arreglo paciente
+        arrayPaciente.push(
+            new Array(nombre, apellido, fechaNacimiento, sexo, labelPais, direccion)
+        );
+
+        //Asignando un mensaje a nuestra notificacion
+        mensaje.innerHTML = "Se ha registrado un nuevo paciente";
+        //Llamando al componente de Bootstrap
+        toast.show();
+
+        //Limpiando formulario
+        limpiarForm();
+    } else {
+        //Asignando un mensaje a nuestra notificacion
+        mensaje.innerHTML = "Faltan campos por completar";
+        //Llamando al componente de Bootstrap
+        toast.show();
+    }
+};
+
+function ImprimirFilas() {
+    let $fila = "";
+    let contador = 1;
+
+    arrayPaciente.forEach((element) => {
+        $fila += `<td class="text-center fw-bold">${contador}</td>
+                <td>${element[0]}</td>
+                <td>${element[1]}</td>
+                <td>${element[2]}</td>
+                <td>${element[3]}</td>
+                <td>${element[4]}</td>
+                <td>${element[5]}</td>
+                <td>
+                    <button id="idBtnEditar${contador}" type="button" class="btn btn-primary" alt="Eliminar">
+                        <i class="bi bi-pencil-square"></i>
+                    </button>
+                    <button id="idBtnEliminar${contador}" type="button" class="btn btn-danger" alt="Editar">
+                        <i class="bi bi-trash3-fill"></i>
+                    </button>
+                </td>
+            </tr>`;
+        contador++;
+    });
+    return $fila;
+}
+
+const imprimirPacientes = () => {
+    let $table = `<div class="table-responsive">
+    <table class="table table-striped table-hover table-bordered">
+        <tr>
+            <th scope="col" class="text-center" style="width:5%">#</th>
+            <th scope="col" class="text-center" style="width:15%">Nombre</th>
+            <th scope="col" class="text-center" style="width:15%">Apellido</th>
+            <th scope="col" class="text-center" style="width:10%">Fecha nacimiento</th>
+            <th scope="col" class="text-center" style="width:10%">Sexo</th>
+            <th scope="col" class="text-center" style="width:10%">Pais</th>
+            <th scope="col" class="text-center" style="width:25%">Direccion</th>
+            <th scope="col" class="text-center" style="width:10%">Opciones</th>
+        </tr>
+        ${ImprimirFilas()}
+    </table>
+    </div>`;
+
+    document.getElementById("idTablaPacientes").innerHTML = $table;
+};
+
+const addPais = () => {
+    let contadorGlobalOption = cmbPais.children.length;
+    let paisNew = inputNombrePais.value;
+
+    if (paisNew != "") {
+        // Creando nuevo option con la API DOM
+        let option = document.createElement("option");
+        option.textContent = paisNew;
+        option.value = contadorGlobalOption + 1;
+
+        //Agregando el nuevo option en el select
+        cmbPais.appendChild(option);
+
+        //Asignando un mensaje a nuestra notificacion
+        mensaje.innerHTML = "Pais agregado correctamente";
+        //Llamando al componente de Bootstrap
+        toast.show();
+    } else {
+        //Asignando un mensaje a nuestra notificacion
+        mensaje.innerHTML = "Faltan campos por completar";
+        //Llamando al componente de Bootstrap
+        toast.show();
+    }
+};
+
+// Agregando eventos a los botones y utilizando funciones tipo flecha
+buttonLimpiarPaciente.onclick = () => {
+    limpiarForm();
+};
+
+buttonAgregarPaciente.onclick = () => {
+    addPaciente();
+};
+
+buttonMostrarPaciente.onclick = () => {
+    imprimirPacientes();
+};
+
+buttonAgregarPais.onclick = () => {
+    addPais();
+};
+
+// Se agrega el focus en el campo nombre pais del modal
+idModal.addEventListener("shown.bs.modal", () => {
+    inputNombrePais.value = "";
+    inputNombrePais.focus();
+});
+
+//Ejecutar funcion al momento de cargar la pagina HTML
+limpiarForm();
