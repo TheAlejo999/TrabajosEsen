7de46db50bf13c6804da33a93aa329d7e81fc9e1
Alejo <20245281@esen.edu.sv>
2025-11-09 11:48:54 -0600

    form

---
+++ Guia6_20245281/js/formulario.js
@@ -0,0 +1,37 @@
+//Accediendo a los elementos html
+const inputNombre = document.getElementById("idTxtNombre");
+const inputApellido = document.getElementById("idTxtApellido");
+const inputFechaNacimiento = document.getElementById("idTxtFechaNacimiento");
+const inputRdMasculino = document.getElementById("idRdMasculino");
+const inputRdFemenino = document.getElementById("idRdFemenino");
+const cmbPais = document.getElementById("idCmbPais");
+const inputDireccion = document.getElementById("idTxtDireccion");
+const inputNombrePais = document.getElementById("idNombrePais");
+
+const buttonAgregarPaciente = document.getElementById("idBtnAgregar");
+const buttonLimpiarPaciente = document.getElementById("idBtnLimpiar");
+const buttonMostrarPaciente = document.getElementById("idBtnMostrar");
+const buttonAgregarPais = document.getElementById("idBtnAddPais");
+
+const notificacion = document.getElementById("idNotificacion");
+// Componente de Bootstrap
+const toast = new bootstrap.Toast(notificacion);
+const mensaje = document.getElementById("idMensaje");
+
+//Componente modal
+const idModal = document.getElementById("idModal");
+
+//Arreglo global de pacientes
+let arrayPaciente = [];
+
+const limpiarForm = () => {
+    inputNombre.value = "";
+    inputApellido.value = "";
+    inputFechaNacimiento.value = "";
+    inputRdMasculino.checked = false;
+    inputRdFemenino.checked = false;
+    cmbPais.selectedIndex = 0;
+    inputDireccion.value = "";
+    inputNombrePais.value = "";
+    inputNombre.focus();
+}
